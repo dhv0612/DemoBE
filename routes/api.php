409e0ffd3da257ccf4967250d1aaadb7a1975b78
dhv0612 <dhv0612@gmail.com>
2024-11-23 10:34:37 +0700

    update db & init api resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -7,5 +8,5 @@ use App\Http\Controllers\AuthController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    Route::resource('customers', CustomerController::class);
 });
